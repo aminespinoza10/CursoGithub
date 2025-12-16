8009a92a2e03739debf3fa272ef7e9cd58c0b341
Amin Espinoza <amin.espinoza@outlook.com>
2025-12-16 15:48:18 -0600

    Nuevo titulo

--- aminesApp/src/App.tsx
+++ aminesApp/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Mi clase de Githubcon Miranda Espinoza</h1>
+      <h1>Mi clase de Github desde Platzi con Miranda Espinoza</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
